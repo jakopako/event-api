a44cb552fe6653eab1a87386a9f3a9200da76404
jakopako <jakob.dhondt@web.de>
2021-10-09 15:08:15 +0200

    basic auth

--- routes/concerts.go
+++ routes/concerts.go
@@ -1,11 +1,19 @@
 package routes
 
 import (
+	"os"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
 	"github.com/jakopako/croncert-api/controllers"
 )
 
 func ConcertsRoute(route fiber.Router) {
+	auth := basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			os.Getenv("API_POST_USER"): os.Getenv("API_POST_PASSWORD"),
+		},
+	})
 	route.Get("/", controllers.GetAllConcerts)
-	route.Post("/", controllers.AddConcert)
+	route.Post("/", auth, controllers.AddConcert)
 }
